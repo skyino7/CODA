e4425b589a50597d3735558d943c721a32fd1b95
Seth Ketsi <christambassador12@gmail.com>
2024-01-05 12:20:33 +0000

    side menu working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Script
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
